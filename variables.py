051a9fe308f0f4b8c984a8d6ac1708269d6409a2
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-09-09 09:29:36 -0400

    Add user Input to variables.py

--- variables.py
+++ variables.py
@@ -1,6 +1,17 @@
-# Hashtags are symbols to create comments
-
 fname = "Towaf" #String
 age = 16 #Int Number
 male = True #Boolean
 balance = 77.77 #Float Number
+
+print(13) #Prints a Number
+print("We are the Best") #Prints a String
+
+#Print Variable Data
+print(fname)
+print(age)
+print(male)
+print(balance)
+
+#Recieve and Output user input
+question = input("What day is it?")
+print(fname, "Happy " + question + "!")
