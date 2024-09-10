335a5fc78b5d25730ecdc1b83d821900b7a0a797
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-09-10 08:50:19 -0400

    Update and rename input.py to CFU-1.py

--- CFU-1.py
+++ CFU-1.py
@@ -1,4 +1,9 @@
+# Variables storing user input
 fname = input("What is your first name?")
 lname = input("What is your last name?")
+
+# Combining userInput variables to store within one variable
 full_name = fname + " " + lname
+
+# Displaying information in the output
 print("Hello " + full_name + "!")
