2e44bbd5584a60328e7f422ef78a8f8a0027db5a
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-09-16 09:33:31 -0400

    Import year

--- unit1/CFU-2.py
+++ unit1/CFU-2.py
@@ -3,8 +3,14 @@
 # Towaf Hossain
 
 
+from datetime import datetime
+
+current_datetime = datetime.now()
+current_year = current_datetime.year
 fname = str(input("Enter your first name"))
 lname = str(input("Enter your last name"))
 age = int(input("How old are you?"))
 soccer_time = int(input("How many years have you been playing soccer?"))
-print("Hello! " + str(fname) + " " + str(lname) + " It says here that you're " + str(age) + " years old! " + "You've been playing soccer for " + str(soccer_time) + " years")
+soccer_years = current_year - soccer_time
+
+print("Hello! " + str(fname) + " " + str(lname) + " You're " + str(age) + " years old! \n" + "You've played soccer for " + str(soccer_time) + " years. \nYou've been playing since " + str(soccer_years) + "!")
