b29702dfe769c9b5a8e98feb2638d87916714e49
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-09-17 08:08:11 -0400

    Create 1-sillysentences.py

---
+++ unit1/projectstem/1-sillysentences.py
@@ -0,0 +1,20 @@
+# 9/17/24
+# 1-2
+# Towaf Hossain
+
+print("Let's play Silly Sentences!")
+
+name = input("Enter a Name: ")
+adj1 = input("Enter an adjective: ")
+adj2 = input("Enter an adjective: ")
+adv = input("Enter an adverb: ")
+food1 = input("Enter a food: ")
+food2 = input("Enter another food: ")
+noun = input("Enter a noun: ")
+place = input("Enter a place: ")
+verb = input("Enter a verb: ")
+
+print(name + " was planning a dream vacation to " + place + ".")
+print(name + " was especially looking forward to trying the local \ncuisine, including " + adj1 + " " + food1 + " " + food2 + ".")
+print(name + " will have to practice the language " + adv + " to make it easier to "+ verb + " with people")
+print(name + " has a long list of sights to see, including the " + noun + " museum and the " + adj2 + " park")
