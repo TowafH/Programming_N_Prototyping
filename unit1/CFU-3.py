3dc5f20350a9bc349f27244c12679b4fccabd73c
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-09-17 15:34:09 -0400

    Create CFU-3.py

---
+++ unit1/CFU-3.py
@@ -0,0 +1,14 @@
+# 9/17/24
+# 1-2
+# Towaf Hossain
+
+num1 = int(input("Enter a Number: "))
+num2 = int(input("Enter another Number: "))
+
+print("Welcome to the World's Greatest Calculator! \n")
+
+print("Addition: " + str(num1 + num2))
+print("Subtraction: " + str(num1 - num2))
+print("Multiplication: " + str(num1 * num2))
+print("Division: " + str(num1 / num2))
+print("Modulos: " + str(num1 % num2))
