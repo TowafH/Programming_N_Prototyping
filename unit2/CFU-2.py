442a59f672f81d968b5f3167201ad52e0a94746b
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-09-23 15:41:14 -0400

    Update CFU-2.py

--- unit2/CFU-2.py
+++ unit2/CFU-2.py
@@ -1,11 +1,13 @@
+# 9/23/24
+# 1-2
+# Towaf Hossain
+
 import math
 
 user_name = input("What is your name?")
 a = int(input("Enter a-value"))
 b = int(input("Enter b-value"))
 c = int(input("Enter c-value"))
-top_of_quadratic = (-b + math.sqrt(b**2 - 4(a*c)))
-bottom_of_quadratic = 2*a
-
-x = top_of_quadratic / bottom_of_quadratic
-print(x)
+x1 = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+x2 = (-b - math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+print("x1 =", x1, "\n" + "x2 =", x2)
