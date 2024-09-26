0ab60b49968286f9623291a6e0dbb4b3782c5303
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-09-26 12:39:05 -0400

    Update CFU-4.py

--- unit2/CFU-4.py
+++ unit2/CFU-4.py
@@ -5,9 +5,12 @@
 import math
 
 user_name = input("What is your name?")
+print("Welcome to the Quadratic Formula Calculator! \n")
 a = int(input("Enter a-value"))
 b = int(input("Enter b-value"))
 c = int(input("Enter c-value"))
+print("Quadratic Formula Entered: " + str(a) + "x^2 + " + str(b) + "x + " + str(c) + "\n")
 x1 = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
 x2 = (-b - math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+print(user_name + ", Here is your answer!")
 print("x1 =", x1, "\n" + "x2 =", x2)
