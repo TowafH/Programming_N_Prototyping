56461c1b51abb7a07f2f8050b2bb1ac81bb51d43
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-10-10 08:28:36 -0400

    Update CFU-7.py

--- unit3/CFU-7.py
+++ unit3/CFU-7.py
@@ -1,6 +1,16 @@
 # 10/10/24
 # PD 1-2
 # Towaf Hossain
+# CFU-7 Functions+Math
+
+'''
+Create a program that asks the user for three numbers.The main program should send those three numbers as arguments to a function to calculates and prints 
+“The sum of your three numbers is:  XXX”
+
+The main program should then send those same numbers to a second function which calculates and prints 
+“The average of your three numbers is:  XXX”
+'''
+
 
 print("Welcome to the Addition & Average Calculator!\n")
 
