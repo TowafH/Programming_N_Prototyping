2cc65918e72046c6f5fbc0cb10e4f30654b8b8a5
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-10-10 08:18:01 -0400

    Create CFU-7.py

---
+++ unit3/CFU-7.py
@@ -0,0 +1,24 @@
+print("Welcome to the Addition & Average Calculator!\n")
+
+def calculate_addition(x1, x2, x3):
+    sum = x1 + x2 + x3
+    print("Addition:", x1, "+", x2, "+", x3)
+    print("The sum of your three numbers is:", sum, "\n")
+    return sum
+
+def calculate_average(sum):
+    average = sum / 3
+    print("Average Calculation:", sum, "/ 3")
+    print("The average of your three numbers is:", average)
+    return average
+
+# User input
+num1 = int(input("Enter a Number:"))
+num2 = int(input("Enter a Number:"))
+num3 = int(input("Enter a Number:"))
+
+# Calculate Sum
+sum = calculate_addition(num1, num2, num3)
+
+# Calculate Average
+calculate_average(sum)
