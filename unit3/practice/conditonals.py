521968bb1c58bd8b5e4351b7449a9aad7dcfb4c9
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-10-10 09:34:40 -0400

    Create conditonals.py

---
+++ unit3/practice/conditonals.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter a Number: "))
+
+if num1 > 5:
+    print(num1, "is bigger than 5\n")
+
+
+
+num1 = int(input("Enter a Number: "))
+
+if num1 >= 5:
+    print(num1, "is greater than or equal to 5")
+else:
+    print(num1, "isn’t greater than or equal to 5")
