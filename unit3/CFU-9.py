c63896f310b0cbb202a97782b4998563514f5b83
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-10-17 08:30:32 -0400

    Create CFU-9.py

---
+++ unit3/CFU-9.py
@@ -0,0 +1,23 @@
+import random
+
+
+print("Welcome to the Guessing Game!\n")
+rolls = int(input("How many rolls do you want?"))
+score = 0
+
+# Use a while loop until there are no more rolls left
+while rolls > 0:
+    # Generate a Random Number
+    random_number = random.randint(1,6)
+    guess = int(input("Guess the Number between 1-6"))
+    
+    # Conditonal Statement
+    if guess == random_number:
+        score += 6
+        print(f"You guessed {random_number} correctly! You gained 6 points")
+    elif guess != random_number:
+        score -= 1
+        print(f"You guessed {random_number} incorrectly! You lost 1 point.")
+    rolls -= 1
+    
+print(f"\nYour final score: {score}")
