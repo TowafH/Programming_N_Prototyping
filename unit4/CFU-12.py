50aa3c7da4d856e519b3f99e67add46c98072c7a
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-10-30 08:20:56 -0400

    Update CFU-12.py

--- unit4/CFU-12.py
+++ unit4/CFU-12.py
@@ -1,19 +1,39 @@
-# 10/29/24
+# 10/30/24
 # PD 1-2
 # Towaf Hossain
+ 
+#V1
+def version1():
+    password = "simonsnyc"
+    user_guess = input("Enter the Password")
+    
+    while user_guess != password:
+        print("Wrong Password")
+        user_guess = input("Enter a Password")
+    print("Correct! You May Enter!")
 
-password = "simonsnyc"
-guess = ""
-num_guess = 0
+#V2
+def version2():
+    password = "simonsnyc"
+    user_guess = ""
+    num_guess = 0
 
-while guess != password and num_guess < 3:
+    while user_guess != password and num_guess < 3:
 
-    guess = input("Enter a Password!")
-    num_guess += 1
+        user_guess = input("Enter a Password!")
+        num_guess += 1
 
-    if guess == password:
-        print("Correct, You may enter!")
-    elif num_guess == 3:
-        print("You didn't get it in 3 tries! Try Again Later!")
-    else:
-        print(f"Wrong Password - Try: #{num_guess}")
+        if user_guess == password:
+            print("Correct, You may enter!")
+        elif num_guess == 3:
+            print("You didn't get it in 3 tries! Try Again Later!")
+        else:
+            print(f"Wrong Password - Try: #{num_guess}")
+
+# Give user the option to choose which version
+version = int(input("Which version would you like to play | 1 or 2 |"))
+
+if version == 1:
+    version1()
+elif version == 2:
+    version2()
