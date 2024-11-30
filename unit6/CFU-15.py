5d40e585eaac676adbe7a7bdee560cf0c4fa3906
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-11-30 12:24:37 -0500

    Update CFU-15.py

--- unit6/CFU-15.py
+++ unit6/CFU-15.py
@@ -1,20 +1,17 @@
+# Towaf Hossain
+# PD 1-2
+# 11/30/24
+
 import simplegui
 
 def draw_handler(canvas):
-    #canvas.draw_polygon([(x1, y1), (x2,y2), (x3,y3)], line_width, "color")
-    canvas.draw_polygon([(80, 50), (80,75), (80,50)], 2, "Black") # Eye1
-    canvas.draw_polygon([(120, 50), (120,75), (120,50)], 2, "Black") # Eye2
-    canvas.draw_polygon([(95,75), (105,80), (95, 90)], 2, "Black") # Nose
-    canvas.draw_polygon([(75, 100), (100,125), (125,100)], 2, "Black") # Mouth
+    canvas.draw_polygon([(70, 60), (75, 50), (85, 50), (90, 60), (85, 70), (75, 70)], 2, "Black") # Eye 1
+    canvas.draw_polygon([(110, 60), (115, 50), (125, 50), (130, 60), (125, 70), (115, 70)], 2, "Black") # Eye 2
+    canvas.draw_polygon([(95, 75), (105, 80), (95, 90)], 2, "Black")  # Nose
+    canvas.draw_polygon([(75, 100), (100, 125), (125, 100)], 2, "Black")  # Mouth
 
 # Code to Modify the Frame
-    # Creates the frame w/ Title & Dimensions
 frame = simplegui.create_frame("CFU 15 - Happy Shapes", 200, 200)
-    # Modify the background color of the frame
 frame.set_canvas_background("White")                         
-    # Set the draw handler
 frame.set_draw_handler(draw_handler)
-
-
-# Nothing will run until we .start() the frame
 frame.start()
