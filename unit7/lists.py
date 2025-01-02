afb5bc6754db90abd596c9b57eb387dbb20b6739
Towaf H <144939977+TowafH@users.noreply.github.com>
2025-01-02 09:26:38 -0500

    Create lists.py

---
+++ unit7/lists.py
@@ -0,0 +1,17 @@
+# Towaf Hossain
+# PD 1-2
+# 12/19/24
+
+'''
+favorites = ["Cristiano Ronaldo", 7, "Soccer"]
+print("Access Index:", favorites[0], "\n")
+
+students = ["Devesh", "Towaf", "Vashti", "Syeda"]
+print("Index Ranges:" , students[2:4], "\n")
+
+for student in students:
+    print(student, "is a Student")
+'''
+
+people = ["Brithany", "Rohaan", "Aliyah", "David", "Micah", "Tasfia", "Rafael", "Nabila", "Nataly", "Michael", "Betsy", "Devesh", "Towaf", "Vashti", "Syeda"]
+items = ["Soda", "Grapes", "Chocolate", "Chips", "Donuts", "Candy"]
