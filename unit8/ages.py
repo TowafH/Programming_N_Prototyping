98cca43857d09f16a13e3092bc19b3b8c14901ed
Towaf H <144939977+TowafH@users.noreply.github.com>
2025-01-07 08:41:27 -0500

    Create ages.py

---
+++ unit8/ages.py
@@ -0,0 +1,7 @@
+groceries = ["Eggs", "Apples", "Milk", "Bread"]
+students = ["Brithany", "Rohaan", "Jannatul", "Aliyah", "David", "Micah", "Armina", "Tasfia", "Rafael", "Nabila", "Nataly", "Michael", "Betsy", "Devesh", "Towaf", "Vashti", "Syeda"]
+age = [18, 16.6, 16.2, 16.1, 17.2, 16.4, 16.1, 16.2, 17.2, 16.5, 16.1, 16.3, 16.2, 16.1, 16.6, 17.4, 16.5]
+# I'm 16.6 years old
+
+print(groceries[0])
+print(str(students[0]) + " is " + str(age[0]) + " years old")
