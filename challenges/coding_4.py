54ec6304577288be9d4b5b58d3bddce4e4d583d8
Towaf H <144939977+TowafH@users.noreply.github.com>
2025-03-03 19:48:22 -0500

    Create coding_4.py

---
+++ challenges/coding_4.py
@@ -0,0 +1,26 @@
+# Towaf Hossain
+# PD 1-2
+# 3/5/25
+
+def row():
+    print('+', '-', '-', '-', '-', '+', '-', '-', '-', '-', '+')
+def column():
+    print("|        ", "|        ", "|        ")
+
+
+row()
+column()
+column()
+column()
+column()
+row()
+column()
+column()
+column()
+column()
+row()
+column()
+column()
+column()
+column()
+row()
