af187cba8ce8c6960e6c8dac0fa6aef43bb04a64
Towaf H <144939977+TowafH@users.noreply.github.com>
2025-03-06 10:06:58 -0500

    Update coding_5.py

--- challenges/coding_5.py
+++ challenges/coding_5.py
@@ -5,8 +5,15 @@
 
 # Fermat's Last Theorem | a**n + b**n = c**n
 def check_fermat(a, b, c, n):
-    if n > 2:
-    	a_and_b = (a ** n) + (b ** n)
+    if n <= 2:
+        print("Fermat's Last Theorem applies only for n > 2.")
+    elif a > 0 and b > 0 and c > 0 and n > 2:
+        a_and_b = (a ** n) + (b ** n)
+        c_and_n = c ** n
+		# Check Fermat's Theorem
+        if a_and_b == c_and_n:
+            print("Holy smokes, Fermat was wrong!")
+        else:
+            print("Nope, that doesn't work")
     
-    
-check_fermat(3, 5, 4, 3)
+check_fermat(3, 3, 3, 3)
