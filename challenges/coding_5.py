95e638a7a53df9ca680ef0650d7b0a07ab1a0ecd
Towaf H <144939977+TowafH@users.noreply.github.com>
2025-03-06 08:54:48 -0500

    Create coding_5.py

---
+++ challenges/coding_5.py
@@ -0,0 +1,12 @@
+# Towaf Hossain
+# PD 1-2
+# 3/6/25
+# PY Coding Challenge 5
+
+# Fermat's Last Theorem | a**n + b**n = c**n
+def check_fermat(a, b, c, n):
+    if n > 2:
+    	a_and_b = (a ** n) + (b ** n)
+    
+    
+check_fermat(3, 5, 4, 3)
