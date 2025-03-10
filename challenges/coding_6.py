ec5eb6e66fa3d6a3850a78cc8453ad4cf0679051
Towaf H <144939977+TowafH@users.noreply.github.com>
2025-03-10 08:16:46 -0400

    Create coding_6.py

---
+++ challenges/coding_6.py
@@ -0,0 +1,17 @@
+# Towaf Hossain
+# PD 1-2
+# 3/10/25
+
+def is_triangle(n1, n2, n3):
+    if n1 == n2 and n2 == n3 and n1 == 3:
+        print("Yes")
+    else:
+        print("No")
+
+def prompt():
+    user1 = int(input("Enter #1"))
+    user2 = int(input("Enter #2"))
+    user3 = int(input("Enter #3"))
+    is_triangle(user1, user2, user3)
+    
+prompt()
