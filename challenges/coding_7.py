c2c88e6ac704ba468c9b2a5285437b429efee7ca
Towaf H <144939977+TowafH@users.noreply.github.com>
2025-03-14 08:36:33 -0400

    Update coding_7.py

--- challenges/coding_7.py
+++ challenges/coding_7.py
@@ -4,10 +4,48 @@
 # 3/14/25
 # PY-Challenge 7
 
+
 import turtle
 
-def square(turtle):
+# Base Code
+def draw(t, length, n):
+    if n == 0:
+        return
+    angle = 50
+    t.forward(length*n)
+    t.left(angle)
+    draw(t, length, n-1)
+    t.right(2*angle)
+    draw(t, length, n-1)
+    t.right(angle)
+    t.backward(length*n)
+
+# Forward Function w/ turtle and number as parameters
+def fd(t, n):
+  t.forward(n)
+
+# Left Function w/ turtle and number as parameters
+def lt(t, n):
+  t.left(n)
+
+# Square function
+def square(t):
   for i in range(4):
-    turtle.forward(90)
-    turtle.left(90)
-square(turtle)
+    fd(t, 90)
+    lt(t, 90 )
+
+# Setup screen
+screen = turtle.Screen
+# Create the Turtle
+t = turtle.Turtle()
+# Base Speed
+t.speed(0)
+
+# Function calls
+draw(t, 10, 5)
+
+# Create bob variable to pass as an argument
+bob = turtle.Turtle()
+square(bob)
+
+screen.mainloop
