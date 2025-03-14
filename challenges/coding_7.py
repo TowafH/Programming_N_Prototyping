b570057f5527f8fdedb72784bfad3e5f62b2dc56
Towaf H <144939977+TowafH@users.noreply.github.com>
2025-03-14 08:20:25 -0400

    Create coding_7.py

---
+++ challenges/coding_7.py
@@ -0,0 +1,13 @@
+# https://trinket.io/turtle/6febe4578276
+# Towaf Hossain
+# PD 1-2
+# 3/14/25
+# PY-Challenge 7
+
+import turtle
+
+def square(turtle):
+  for i in range(4):
+    turtle.forward(90)
+    turtle.left(90)
+square(turtle)
