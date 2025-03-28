fea96c2e0aa3343c6bc39b53a75b6234ebe465ff
Towaf H <144939977+TowafH@users.noreply.github.com>
2025-03-28 08:58:59 -0400

    Update coding_13.py

--- challenges/coding_13.py
+++ challenges/coding_13.py
@@ -1,77 +1,31 @@
-# Towaf Hossain
-# PD 1-2
-
 import turtle
+import math
 
-def draw(t, length, n):
-    if n == 0:
-        return
-    angle = 50
-    t.forward(length * n)
-    t.left(angle)
-    draw(t, length, n - 1)
-    t.right(2 * angle)
-    draw(t, length, n - 1)
-    t.right(angle)
-    t.backward(length * n)
+def iso(T, r, angle):
+  y = r * math.sin(math.radians(angle))
+  T.rt(angle)
+  T.fd(r)
+  T.lt(90 + angle)
+  T.fd(y * 2)
+  T.lt(90 + angle)
+  T.fd(r)
+  T.lt(180 - angle)
 
-# Forward Function w/ turtle and number as parameters
-def fd(t, n):
-    t.forward(n)
+def pie(T, r, n):
+  angle = 360 / n
+  for i in range(n):
+    iso(T, r, angle / 2)
+    T.lt(angle)
 
-# Left Function w/ turtle and number as parameters
-def lt(t, n = 90):
-    t.left(n)
+def pies(T, r, n):
+  pie(T, r, n)
+  T.pu()
+  T.fd(r * 2 + 10)
+  T.pd()
 
-# Right Function w/ turtle and number as parameters
-def rt(t, n = 90):
-    t.right(n)
 
-# Square function
-def square(t, length):
-    for i in range(4):
-        fd(t, length)
-        lt(t)
-
-# Polygon function
-def polygon(t, length, n):
-    angle = 360 / n
-    for i in range(n):
-        fd(t, length)
-        lt(t, angle)
-
-# Setup screen
-screen = turtle.Screen()
-
-# Create the Turtle
-t = turtle.Turtle()
-# Base Speed
-t.speed(0)
-
-# Test the polygon function with various n values
-bob = turtle.Turtle()
-bob.color("red")
-lt(bob, 60)
-fd(bob, 50)
-bob.backward(100)
-fd(bob, 50)
-lt(bob, 50)
-fd(bob, 50)
-bob.backward(100)
-fd(bob, 50)
-lt(bob, 60)
-fd(bob, 50)
-bob.backward(100)
-lt(bob, 60)
-fd(bob, 50)
-lt(bob, -55)
-fd(bob, 50)
-rt(bob, 65)
-fd(bob, 55)
-rt(bob, 60)
-fd(bob, 50)
-rt(bob, 55)
-fd(bob, 50)
-rt(bob, 65)
-fd(bob, 60)
-screen.mainloop()
+T = turtle.Turtle()
+pies(T, 10, 5)
+pies(T, 10, 6)
+pies(T, 10, 7)
+pies(T, 10, 8)
